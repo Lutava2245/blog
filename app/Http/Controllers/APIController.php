cbc7376167f992abc2ec03ec285e7cb2edee7270
Murillo <murillo.lds01@gmail.com>
2025-06-15 21:19:28 -0300

    Atualização dos métodos da APIController

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -15,27 +15,39 @@ class APIController extends Controller
     // Criar uma nova postagem
     public function store(Request $request)
     {
-        $postagem = Postagem::create($request::all());
+        $postagem = Postagem::create($request->all());
         return $postagem;
     }
 
     // Exibir uma postagem específica
-    public function show($postagem)
+    public function show($id)
     {
+        $postagem = Postagem::findOrFail($id);
+
+        $key = "visualizacao_{$id}";
+
+        if (!session()->has($key) || now()->diffInSeconds(session($key)) > 30) {
+            $postagem->increment('visualizacoes');
+            session([$key => now()]);
+        }
+
         return $postagem;
     }
 
     // Atualizar uma postagem
-    public function update(Request $request, Postagem $postagem)
+    public function update(Request $request, $id)
     {
-        $postagem->update($request::all());
+        $postagem = Postagem::findOrFail($id);
+        $postagem->update($request->all());
         return $postagem;
     }
 
     // Deletar uma postagem
-    public function destroy(Postagem $postagem)
+    public function destroy($id)
     {
+        $postagem = Postagem::findOrFail($id);
         $postagem->delete();
-        return $postagem;
+        return response()->json(['message' => 'Postagem excluída com sucesso']);
+
     }
 }
